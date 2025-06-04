5aac48ca6eb00db09594619060529080f338a48a
malcor21 <reedmalcolm2025@u.northwestern.edu>
2025-06-03 20:05:26 -0500

    s3: bt lassovars tuning

--- submissions/submission-3/34.1_tune_bt_lassovars.R
+++ submissions/submission-3/34.1_tune_bt_lassovars.R
@@ -18,8 +18,8 @@ load(here("submissions/submission-3/data/reg_folds.rda"))
 # load recipe
 load(here("submissions/submission-3/recipes/recipe_tree_lassovars.rda"))
 
-# parlassovarsel processing ----
-num_cores <- parlassovarsel::detectCores(logical= TRUE)
+# parallel processing ----
+num_cores <- parallel::detectCores(logical= TRUE)
 plan(multisession, workers = num_cores - 2)
 
 # set seed
@@ -42,7 +42,7 @@ bt_spec <-
 bt_wflow <- 
   workflow() %>% 
   add_model(bt_spec) %>% 
-  add_recipe(recipe_tree)
+  add_recipe(recipe_tree_lassovars)
 
 # hyperparameter tuning values ----
 
@@ -52,7 +52,7 @@ bt_params <- extract_parameter_set_dials(bt_spec) %>%
     mtry = mtry(c(2, 30)), 
     min_n = min_n(c(2, 20)),
     trees = trees(c(1200, 2800)),
-    learn_rate = learn_rate(c(-4, -0.5)),
+    learn_rate = learn_rate(c(-2.5, -0.5)), # fixing this based on all vars result
     loss_reduction = loss_reduction(c(-15, -3)),
     tree_depth = tree_depth(c(4, 20))
   )
@@ -70,7 +70,7 @@ tic.clearlog() # clear log
 tic("s3_bt_lassovars") # start clock
 
 # tuning code in here
-tune_bt_lassovars <- bt_wflow %>%  # name was a mistake, will fix next time
+tune_bt_lassovars_true <- bt_wflow %>%  # name was a mistake, will fix next time
   tune_grid(
     resamples = reg_folds,
     grid = bt_grid,
@@ -92,7 +92,7 @@ tictoc_bt <- tibble(
 
 # write out results (fitted/trained workflows & runtime info) ----
 save(
-  tune_bt_lassovars, # bundle tuning object and time
+  tune_bt_lassovars_true, # bundle tuning object and time
   tictoc_bt,
   file = here("submissions/submission-3/results/tune_bt_lassovars.rda")
 )
---
+++ submissions/submission-3/35_model_evaluation.R
@@ -0,0 +1,33 @@
+## Stat 301-3 Prediction Problem - Regression
+# 35: model eval ----
+
+# Load package(s)
+library(tidymodels)
+library(tidyverse)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
+
+# load result objects ----
+paths <- list.files(
+  path = here("submissions/submission-3/results/"), 
+  full.names = TRUE,
+  pattern = "tune"
+)
+for (path in paths){
+  load(path)
+}
+
+# bt analysis ----
+tune_bt_lassovars %>% 
+  autoplot(metric = "mae")
+# mtry: not super clear. Try a little larger (2, 30)
+# trees: seem to decrease. Go out further (1200, 2400)
+# learn_rate: try smaller (-4, -0.5)
+# min_n: smaller seems best. (2, 20)
+# loss_reduction: smaller is definitely best - try below -10 (-15, -3)
+# tree_depth: maybe increasing is best? try (4, 20)
+
+tune_bt_lassovars %>% 
+  select_best(metric = "mae")
