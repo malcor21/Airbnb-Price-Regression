95a54dddcf43a409768ebb277083ee7292ab1086
malcor21 <reedmalcolm2025@u.northwestern.edu>
2025-06-04 20:39:05 -0500

    report first draft

--- submissions/submission-2/25_model_evaluation.R
+++ submissions/submission-2/25_model_evaluation.R
@@ -20,14 +20,20 @@ for (path in paths){
 }
 
 # bt analysis ----
-tune_bt_lassovars %>% 
-  autoplot(metric = "mae")
+s2_bt_autoplot <- tune_bt_lassovars %>% 
+  autoplot(metric = "mae") +
+  guides(shape = "none") +
+  labs(
+    title = "Submission Set 2 Boosted Trees Tuning"
+  )
 # mtry: not super clear. Try a little larger (2, 30)
 # trees: seem to decrease. Go out further (1200, 2400)
 # learn_rate: try smaller (-4, -0.5)
 # min_n: smaller seems best. (2, 20)
 # loss_reduction: smaller is definitely best - try below -10 (-15, -3)
 # tree_depth: maybe increasing is best? try (4, 20)
+
+ggsave(s2_bt_autoplot, filename = here("figures/s2_bt_autoplot.jpg"))
 
 tune_bt_lassovars %>% 
   select_best(metric = "mae")
